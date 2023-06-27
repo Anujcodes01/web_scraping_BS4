da4e35cb77c9b74d41ed952d3a72f3eaab989213
Anujdh8755 <anujdhyani232@gmail.com>
2023-06-27 15:57:20 +0530

    connection with mongodb

--- app.py
+++ app.py
@@ -3,17 +3,19 @@ from flask_cors import CORS,cross_origin
 import requests
 from bs4 import BeautifulSoup as bs
 from urllib.request import urlopen as uReq
-import logging
 import pymongo
-logging.basicConfig(filename="scrapper.log" , level=logging.INFO)
+import csv
 
-app = Flask(__name__)
+application = Flask(__name__) # initializing a flask app
+app=application
 
-@app.route("/", methods = ['GET'])
-def homepage():
+@app.route('/',methods=['GET'])  # route to display the home page
+@cross_origin()
+def homePage():
     return render_template("index.html")
 
-@app.route("/review" , methods = ['POST' , 'GET'])
+@app.route('/review',methods=['POST','GET']) # route to show the review comments in a web UI
+@cross_origin()
 def index():
     if request.method == 'POST':
         try:
@@ -30,84 +32,68 @@ def index():
             prodRes = requests.get(productLink)
             prodRes.encoding='utf-8'
             prod_html = bs(prodRes.text, "html.parser")
-            print(prod_html)
             commentboxes = prod_html.find_all('div', {'class': "_16PBlm"})
 
             filename = searchString + ".csv"
-            fw = open(filename, "w")
-            headers = "Product, Customer Name, Rating, Heading, Comment \n"
-            fw.write(headers)
-            reviews = []
-            for commentbox in commentboxes:
-                try:
-                    #name.encode(encoding='utf-8')
-                    name = commentbox.div.div.find_all('p', {'class': '_2sc7ZR _2V5EHH'})[0].text
+            with open(filename, "w", newline='', encoding='utf-8') as fw:
+                headers = ["Price","Product","Customer Name", "Rating","Heading","Comment"]
+                writer = csv.DictWriter(fw, fieldnames=headers)
+                writer.writeheader()
 
-                except:
-                    logging.info("name")
+                reviews = []
+                for commentbox in commentboxes:
+                    try:
+                        price_element = flipkart_html.select('div._25b18c ._30jeq3')[0]
+                        price = price_element.text
+                    except:    
+                        price = 'There is no price'
+                    try:
+                        #name.encode(encoding='utf-8')
+                        name = commentbox.div.div.find_all('p', {'class': '_2sc7ZR _2V5EHH'})[0].text
 
-                try:
-                    #rating.encode(encoding='utf-8')
-                    rating = commentbox.div.div.div.div.text
+                    except:
+                        name = 'No Name'
+
+                    try:
+                        #rating.encode(encoding='utf-8')
+                        rating = commentbox.div.div.div.div.text
 
 
-                except:
-                    rating = 'No Rating'
-                    logging.info("rating")
+                    except:
+                        rating = 'No Rating'
 
-                try:
-                    #commentHead.encode(encoding='utf-8')
-                    commentHead = commentbox.div.div.div.p.text
+                    try:
+                        #commentHead.encode(encoding='utf-8')
+                        commentHead = commentbox.div.div.div.p.text
 
-                except:
-                    commentHead = 'No Comment Heading'
-                    logging.info(commentHead)
-                try:
-                    comtag = commentbox.div.div.find_all('div', {'class': ''})
-                    #custComment.encode(encoding='utf-8')
-                    custComment = comtag[0].div.text
-                except Exception as e:
-                    logging.info(e)
+                    except:
+                        commentHead = 'No Comment Heading'
+                    try:
+                        comtag = commentbox.div.div.find_all('div', {'class': ''})
+                        #custComment.encode(encoding='utf-8')
+                        custComment = comtag[0].div.text
+                    except Exception as e:
+                        print("Exception while creating dictionary: ",e)
 
-                mydict = {"Product": searchString, "Name": name, "Rating": rating, "CommentHead": commentHead,
-                          "Comment": custComment}
-                reviews.append(mydict)
-            logging.info("log my final result {}".format(reviews))
-            
+                    mydict = {"Price": price,"Product": searchString, "Customer Name": name, "Rating": rating, "Heading": commentHead,"Comment": custComment}
+                    reviews.append(mydict)
+                   
+                writer.writerows(reviews)
 
-            # try:
-            #     # Establish the MongoDB connection
-            #     client = pymongo.MongoClient("mongodb+srv://AnujDhyani:dhyanianuj0303@cluster0.oxumags.mongodb.net/?retryWrites=true&w=majority")
-
-            #     # Check connection status
-            #     if client.is_mongos:
-            #         print("Connected to MongoDB cluster successfully.")
-            #     else:
-            #         print("Connection to MongoDB cluster failed.")
-
-            #     # Select the database and collection
-            #     db = client['review_scrap']
-            #     review_col = db['review_scrap_data']
-
-            #     # Insert documents into the collection
-            #     result = review_col.insert_many(reviews)
-
-            #     # Output the number of documents inserted
-            #     print("Inserted", len(result.inserted_ids), "documents into the collection.")
-
-            # except pymongo.errors.PyMongoError as e:
-            #     print("An error occurred:", e)
-
-            
+               
+            client = pymongo.MongoClient("mongodb+srv://@cluster0.oxumags.mongodb.net/?retryWrites=true&w=majority")
+            db = client['flipkart_scrap1']
+            review_col = db['review_scrap_data']
+            review_col.insert_many(reviews)
             return render_template('result.html', reviews=reviews[0:(len(reviews)-1)])
         except Exception as e:
-            logging.info(e)
+            print('The Exception message is: ',e)
             return 'something is wrong'
     # return render_template('results.html')
 
     else:
         return render_template('index.html')
 
-
-if __name__=="__main__":
-    app.run(host="0.0.0.0")
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8000, debug=True)
+	#app.run(debug=True)    
